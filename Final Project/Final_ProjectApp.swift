254a638db55ad995d959d130d5241ac352b5b245
Rania <raniakasuri55555@gmail.com>
2024-06-25 13:24:21 -0400

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Rania Kasuri on 25/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
